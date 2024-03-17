1bb4a3cddf36867fc68b01c21e8a430af581a22e
Chizoba Orakwue <chizyraky@gmail.com>
2024-03-16 20:10:39 -0400

    Initial Commit

---
+++ Lab3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab3
+//
+//  Created by Chizoba Orakwue on 2024-03-16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
